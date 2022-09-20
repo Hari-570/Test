11f8d33f5430c34348d9403205dd28cba4c246d8
HARITHA <oguriharitha456@gmail.com>
2022-09-19 17:56:45 -0700

    First Version

---
+++ src/ListExample.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+class ListExample {
+    public static void main(String args[]) {
+        List<String> h = new ArrayList();
+        h.add("Hari");
+        h.add("Sheeba");
+        h.add("Lavs");
+        System.out.println(h);
+    }
+}
