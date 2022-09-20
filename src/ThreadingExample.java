00bb7a9fa275dfe552d82dcaecda1dc53df906bc
Hari570 <oguriharitha456@gmail.com>
2022-09-20 15:30:56 -0700

    First Version

---
+++ src/ThreadingExample.java
@@ -0,0 +1,9 @@
+public class ThreadingExample {
+    public static void main(String[] args) {
+        ThreadingExample r = new ThreadingExample();
+        for(int i=0;i<=100;i++)
+        {
+            System.out.println("Hari");
+        }
+    }
+}
